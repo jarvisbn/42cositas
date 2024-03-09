d15b57a56c34b37bcfe75fba5d5b27d1a99525ef
jarvisbn <144715747+jarvisbn@users.noreply.github.com>
2024-03-09 13:46:54 +0100

    Update ft_printf.c

--- untitled folder 2/ft_printf.c
+++ untitled folder 2/ft_printf.c
@@ -14,45 +14,6 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-int print_c(int c)
-{
-	return write(1, &c, 1);
-}
-
-int print_s(char *str)
-{
-	int count;
-	while (*str != '\0')
-	{
-		print_c((int)*str);
-		str++;
-		count++;
-	}
-	return count;
-}
-
-int print_digit(long n, int base)
-{
-	int count;
-	char *symbols;
-
-	symbols = "0123456789abcdef";
-	if (n < 0)
-	{
-		write(1, "-", 1);
-		return print_digit(-n, base) + 1;
-	}
-	else if (n < base)
-	{
-		return print_c(symbols[n]);
-	}
-	else
-	{
-		count = print_digit(n / base, base);
-		return count + print_digit(n % base, base);
-	}
-}
-
 int type_format(char type, va_list ap)
 {
 	int total;
